887f730262e4b252a168877ae40307759afd6805
hindaneohub-code <hindaneohub@github.com>
2026-08-08 18:18:35 +0530

    commit 1

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,6 +1,5 @@
 export const ADMIN_WALLETS = [
-    '0x6bc0CE568a12CB0C17576dCdE9a0Cef345B30447',
-    '0x0aFb7cEF6C442Bb2AB94810A7201B34ae1C9333A'
+    '0x6bc0CE568a12CB0C17576dCdE9a0Cef345B30447'
 ].map(a => a.toLowerCase());
 
 export const isAdmin = (address: string | undefined | null) => {
