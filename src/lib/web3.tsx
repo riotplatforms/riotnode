48839deb5ab3e4a9e4919dde63f64a741634fa38
hindaneohub-code <hindaneohub@github.com>
2026-08-10 19:34:26 +0530

    feat: add TrustWallet, MetaMask, Rainbow, Coinbase, SafePal, TokenPocket as featured wallets in AppKit

--- src/lib/web3.tsx
+++ src/lib/web3.tsx
@@ -271,6 +271,14 @@ if (!appKitInitialized) {
         metadata,
         projectId,
         allWallets: 'SHOW',
+        featuredWalletIds: [
+            '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+            'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+            '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369', // Rainbow
+            'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa', // Coinbase
+            '0b415a74b010e646b3a393a1953494349efe26d62a3d7e716e7b45f4b7b4b1d0', // SafePal
+            '8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb60d7', // TokenPocket
+        ],
         enableMobileFullScreen: true,
         features: {
             analytics: true,
